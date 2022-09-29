d6c899f7554b8652e97475b20ea17623f19061c1
seojjin <002seojin@gmail.com>
2022-09-29 14:41:01 +0900

    FBV로 대문 상세페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index), #ip주소/blog
-    path('<int:pk>/', views.single_post_page)
+    #path('', views.index), #ip주소/blog
+    #path('<int:pk>/', views.single_post_page),
+    path('', views.PostList.as_view()),
+    path('<int:pk>/',views.PostDetail.as_view())
  ]
